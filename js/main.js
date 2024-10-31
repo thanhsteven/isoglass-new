1e42fbc52cc5a53e47b12e872afcb275897940b6
thanhsteven <kimthanhpham96@gmail.com>
2024-11-01 00:09:36 +0700

    Udpate souce

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("Xin chào cô chú");
+(function () {
+  const btnMenu = document.querySelector(".header-right-menu");
+  const menu = document.querySelector(".menu");
+  const closeMenu = document.querySelector(".menu-btn-close");
+  const modal = document.querySelector(".modal");
+
+  btnMenu.addEventListener("click", function (event) {
+    menu.classList.toggle("menu-show");
+    console.log(menu);
+  });
+
+  closeMenu.addEventListener("click", function () {
+    menu.classList.toggle("menu-show");
+  });
+
+  function showModal() {}
+})();
+
