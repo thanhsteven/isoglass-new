d2a0df130c1267adc069632bc2b523d04224d1f6
thanhsteven <kimthanhpham96@gmail.com>
2024-11-01 17:59:14 +0700

    Update source

--- js/main.js
+++ js/main.js
@@ -1,18 +1,43 @@
 (function () {
+  // -> Khai báo Biến
   const btnMenu = document.querySelector(".header-right-menu");
   const menu = document.querySelector(".menu");
   const closeMenu = document.querySelector(".menu-btn-close");
   const modal = document.querySelector(".modal");
+  // -> Biến Menu Con
+  // const menuChild = document.querySelector(".menu-child");
+  const menuListItemAll = document.querySelectorAll(".menu-list-item-wrapper");
 
-  btnMenu.addEventListener("click", function (event) {
-    menu.classList.toggle("menu-show");
-    console.log(menu);
+  // console.log(menuChild);
+
+  // ! Đang xử lý
+
+  menuListItemAll.forEach(function (menuItem) {
+    menuItem.addEventListener("click", function (event) {
+      event.stopPropagation();
+      const menuChild = event.target.nextElementSibling;
+      if (menuChild) {
+        menuChild.classList.toggle("menu-child-show");
+      } else {
+        return false;
+      }
+    });
   });
 
-  closeMenu.addEventListener("click", function () {
-    menu.classList.toggle("menu-show");
-  });
+  // -> Sự kiện
+  btnMenu.addEventListener("click", openMenuMobile);
+  closeMenu.addEventListener("click", closeMenuMobile);
+  modal.addEventListener("click", closeMenuMobile);
 
-  function showModal() {}
+  // -> Hàm - Mở/Đóng Menu
+  function openMenuMobile() {
+    menu.classList.toggle("menu-show");
+    modal.classList.toggle("modal-show");
+    document.body.style.setProperty("overflow", "hidden");
+  }
+  function closeMenuMobile() {
+    menu.classList.toggle("menu-show");
+    modal.classList.toggle("modal-show");
+    document.body.removeAttribute("style");
+  }
 })();
-
