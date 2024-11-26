54feaf2cec2a25941b917062c973e7cdaada7b2c
thanhsteven <kimthanhpham96@gmail.com>
2024-11-26 15:54:44 +0700

    Fix menu and modal

--- js/menu-2.js
+++ js/menu-2.js
@@ -42,19 +42,19 @@ console.log("Đã hoàn thành menu");
   }
 
   const btnOpenMenu = document.querySelector(".header-right-menu");
-  const modalMenu = document.querySelector(".menu-modal");
+  const modalFooter = document.querySelector(".footer-modal");
   const btnCloseMenu = document.querySelector(".menu-close");
   btnOpenMenu.addEventListener("click", menuMobile);
   btnCloseMenu.addEventListener("click", menuMobile);
-  modalMenu.addEventListener("click", menuMobile);
+  modalFooter.addEventListener("click", menuMobile);
   function menuMobile() {
     navMenu.classList.toggle("menu-nav-show");
-    modalMenu.classList.toggle("menu-modal-show");
-    if (document.body.style.overflow === "hidden") {
-      document.body.style.removeProperty("overflow");
-      document.body.removeAttribute("style");
+    modalFooter.classList.toggle("footer-modal-show");
+    if (document.documentElement.style.overflow === "hidden") {
+      document.documentElement.style.removeProperty("overflow");
+      document.documentElement.removeAttribute("style");
     } else {
-      document.body.style.setProperty("overflow", "hidden");
+      document.documentElement.style.setProperty("overflow", "hidden");
     }
   }
 
@@ -65,7 +65,7 @@ console.log("Đã hoàn thành menu");
     }
   });
   function resizeMenu() {
-    modalMenu.classList.remove("modal-show");
+    modalFooter.classList.remove("modal-show");
     navMenu.classList.remove("menu-nav-show");
   }
 })();
